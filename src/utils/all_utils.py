0fa2beb3e429618c06bbe8c0f789e631b8ed441a
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-07-06 15:50:20 +0200

    tensorboard callbacks added

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -14,3 +14,7 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         logging.info(f"directory is created at {dir_path}")
+def get_timestamp(name):
+    timestamp=time.asctime().replace(" ","_").replace(":","_")
+    unique_name=f"{name}_at_{timestamp}"
+    return unique_name
