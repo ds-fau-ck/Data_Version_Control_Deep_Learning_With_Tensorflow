8b73dd3ddae51446f390bf3c7d7cd152d46af735
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-07-06 15:32:19 +0200

    stage_03 base file is ready

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,4 +1,6 @@
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import get_VGG_16_model,prepare_model
+from src.utils.callbacks import create_and_save_tensorboard_callback,create_and_save_checkpoint_callback
 import argparse
 import pandas as pd
 import os
@@ -12,8 +14,25 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
 
-def prepare_callbacks(source_download_dir, local_data_dir):
-    pass
+def prepare_callbacks(config_path, params_path):
+    config=read_yaml(config_path)
+
+    artifacts=config["artifacts"]
+    artifacts_dir=artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir=os.path.join(artifacts_dir,artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+
+    checkpoint_dir=os.path.join((artifacts_dir, artifacts["CALLBACKS_DIR"]))
+    callbacks_dir=os.path.join(artifacts_dir, artifacts["HECKPOINT_DIR"])
+
+    create_directory([
+        tensorboard_log_dir,
+        checkpoint_dir,
+        callbacks_dir
+        ])
+    create_and_save_tensorboard_callback(callbacks_dir,tensorboard_log_dir)
+    create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir)
+
+
 
 
 
