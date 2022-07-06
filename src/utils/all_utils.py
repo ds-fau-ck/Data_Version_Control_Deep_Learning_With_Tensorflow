c7ebebb9ac6471ccd0cd396e37b80da8aacdce1b
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-07-06 11:23:10 +0200

    all_utils update

---
+++ src/utils/all_utils.py
@@ -0,0 +1,16 @@
+import yaml
+import os
+import json
+import logging
+import time
+
+def read_yaml(path_to_yaml: str) -> dict:
+    with open(path_to_yaml) as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+    return content
+    
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        logging.info(f"directory is created at {dir_path}")
