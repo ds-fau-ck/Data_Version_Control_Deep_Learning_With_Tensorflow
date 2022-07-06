dd5641bcc9cda13bceb745683936715b0d8385c1
TUCchkul <42653001+TUCchkul@users.noreply.github.com>
2022-07-06 10:02:05 +0200

    stage_01 added

---
+++ src/stage_01_load_save.py
@@ -0,0 +1,31 @@
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import pandas as pd  
+import os
+import shutil
+from tqdm import tqdm
+def copy_file(source_download_dir,local_data_dir):
+    list_of_files=os.listdir(source_download_dir)
+    N=len(list_of_files)
+    for file in tqdm(list_of_files, total=N, desc=f"Copying file from  {source_download_dir}to {local_data_dir}", color="green"):
+        src=os.path.join(source_download_dir,file)
+        dest=os.path.join(local_data_dir,file)
+        shutil.copy(src,dest)
+
+def get_data(config_path):
+    config=read_yaml(config_path)
+    source_download_dir=config["source_download_dir"]
+    local_data_dir=config["local_data_dir"]
+
+    for source_download_dir,local_data_dir in tqdm(zip(source_download_dir,local_data_dir),total=2, desc= "list of Folder", color="red"):
+        create_directory([local_data_dir])
+        copy_file(source_download_dir,local_data_dir)
+        
+
+
+
+if __name__=='__main__':
+    args=argparse.ArgumentParser()
+    args.add_argument("--config","-c",default="config/config.yaml")
+    parsed_args=args.parse_args()
+    get_data(config_path=parsed_args.config)
